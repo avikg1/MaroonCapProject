5152531edb057ad3d3f205c3f3d18c9417c05f79
Avik Garg <avikgarg103@gmail.com>
2024-02-14 20:27:32 -0600

    forgot some code

--- functions/other_functions.py
+++ functions/other_functions.py
@@ -3,15 +3,11 @@ import numpy as np
 
 def read_stock_tickers(filepath):
     tickers = []
-    i = 0
     with open(filepath, 'r') as file:
         for line in file:
-            i += 1
-            if i > 10:
-                break
             stripped = line.strip()
             if stripped not in tickers:
-                tickers.append(line.strip())  # Remove any leading/trailing whitespace or newline chars
+                tickers.append(stripped)
     return tickers
 
 
@@ -36,17 +32,18 @@ def calculate_fama_french_factors(df):
     grouped_by_date = df.groupby('Date')
     
     for date, group in grouped_by_date:
+        valid_group = group.dropna(subset=['Return', 'MarketCap'])
         # Calculate returns and market cap for the group
-        total_market_cap = group['MarketCap'].sum()
-        group['Weight'] = group['MarketCap'] / total_market_cap
-        market_return = (group['Return'] * group['Weight']).sum()
+        total_market_cap = valid_group['MarketCap'].sum()
+        valid_group['Weight'] = valid_group['MarketCap'] / total_market_cap
+        market_return = (valid_group['Return'] * valid_group['Weight']).sum()
         
         # Assign market cap and PE ratio ranks
-        group['MarketCapRank'] = pd.qcut(group['MarketCap'], 2, labels=False)  # 0 is low, 1 is high
-        group['PERank'] = pd.qcut(group['trailingPE'], [0, 0.3, 0.7, 1.0], labels=False)  # 0, 1, 2 for low, middle, high
+        valid_group['MarketCapRank'] = pd.qcut(valid_group['MarketCap'], 2, labels=False)  # 0 is low, 1 is high
+        valid_group['PERank'] = pd.qcut(valid_group['trailingPE'], [0, 0.3, 0.7, 1.0], labels=False)  # 0, 1, 2 for low, middle, high
         
         # Group by ranks within the date group and calculate weighted returns for each subgroup
-        grouped_by_rank = group.groupby(['MarketCapRank', 'PERank'])
+        grouped_by_rank = valid_group.groupby(['MarketCapRank', 'PERank'])
         portfolio_returns = grouped_by_rank.apply(lambda x: (x['Return'] * x['Weight']).sum())
         
         # Calculate SMB and HML for the group
